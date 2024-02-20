470538fef873039c51cb0d361cce30cdd73c5c29
MatthewLeaman <leaman.30@osu.edu>
2024-02-20 11:51:01 -0500

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function Blur() {
+        console.log("Hey! Eyes on me!")
+    }
+    function Focus() {
+        console.log("Good!")
+    }
+    return (
+        <div>
+            <button onFocus={Focus} onBlur={Blur}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
